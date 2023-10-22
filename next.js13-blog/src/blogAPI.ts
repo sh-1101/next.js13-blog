545778e92b68bac0898795a039cd8efdd7eff08a
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-10-22 15:25:20 +0900

    ブログ投稿用APIを作成

--- next.js13-blog/src/blogAPI.ts
+++ next.js13-blog/src/blogAPI.ts
@@ -26,3 +26,24 @@ export const getDetailArticle = async (id: string): Promise<Article> => {
   const article = await res.json();
   return article;
 };
+
+export const createArticle = async (
+  id: string,
+  title: string,
+  content: string
+): Promise<Article> => {
+  const currentDateTime = new Date().toISOString();
+  const res = await fetch(`http://localhost:3001/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, title, content, createdAt: currentDateTime }),
+  });
+
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。");
+  }
+  const newArticle = await res.json();
+  return newArticle;
+};
