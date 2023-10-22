40b55d797cfc9803cd459beca74aa8bd5d598d7a
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-10-22 16:08:57 +0900

    ブログ記事削除用のAPIを作成

--- next.js13-blog/src/blogAPI.ts
+++ next.js13-blog/src/blogAPI.ts
@@ -47,3 +47,16 @@ export const createArticle = async (
   const newArticle = await res.json();
   return newArticle;
 };
+
+export const deleteArticle = async (id: string): Promise<Article> => {
+  const currentDateTime = new Date().toISOString();
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。");
+  }
+  const deleteArticle = await res.json();
+  return deleteArticle;
+};
