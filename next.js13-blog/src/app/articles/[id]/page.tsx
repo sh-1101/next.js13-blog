ab8cb0dddbdaf51902f61951b44d4ec4d89300af
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-10-23 22:22:15 +0900

    記事詳細取得APIをapp/apiで書き換え

--- next.js13-blog/src/app/articles/[id]/page.tsx
+++ next.js13-blog/src/app/articles/[id]/page.tsx
@@ -1,10 +1,16 @@
 import DeleteButton from "@/app/components/DeleteButton";
-import { getDetailArticle } from "@/blogAPI";
 import Image from "next/image";
 import React from "react";
 
 const Article = async ({ params }: { params: { id: string } }) => {
-  const detailArticle = await getDetailArticle(params.id);
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+  const res = await fetch(`${API_URL}/api/blog/${params.id}`, {
+    next: {
+      revalidate: 10,
+    },
+  });
+  const detailArticle = await res.json();
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
